22a15a64928a1856169a223e5c2e06caf03b3cdf
olajidechris <42500217+olajidechris@users.noreply.github.com>
2023-03-18 11:31:45 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,11 +12,11 @@ int main(void)
 {
   int n = 0;
   char lower_case_alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-  while (n < 26))
+  while (n < 26)
     {
-      putchar(lower_case_alphabet[n]);
+      putchar (lower_case_alphabet[n]);
       n++;
     }
-  putchar('\n');
+  putchar ('\n');
   return (0);
 }
